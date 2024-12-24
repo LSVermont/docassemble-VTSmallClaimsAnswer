3e6531f23f0e7036b4ccda71e1aea51424b6da81
Kris Surette <104463248+VTskier@users.noreply.github.com>
2024-12-24 16:17:08 -0500

    Improved text, etc

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -52,8 +52,7 @@ setup(name='docassemble.VTSmallClaimsAnswer',
       author_email='ksurette@legalservicesvt.org',
       license='The MIT License',
       url='https://courtformsonline.org',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
+      packages=find_namespace_packages(),
       install_requires=['docassemble.AssemblyLine>=3.2.0', 'docassemble.VTCertificateOfService', 'docassemble.VTCourtFormsBranding', 'docassemble.VTSharedYMLFile'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/VTSmallClaimsAnswer/', package='docassemble.VTSmallClaimsAnswer'),
