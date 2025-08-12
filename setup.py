246ebe23aa590fafa67178867970c084ec7aaac1
Kris Surette <104463248+VTskier@users.noreply.github.com>
2025-08-12 17:10:56 -0400

    updated text on id: defenses

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.VTSmallClaimsAnswer',
       license='The MIT License',
       url='https://VTLawHelp.org',
       packages=find_namespace_packages(),
-      install_requires=['docassemble.AssemblyLine>=3.2.0', 'docassemble.GithubFeedbackForm>=0.4.1.1', 'docassemble.VTDisclosureOfExemptIncome', 'docassemble.VTFeeWaiverWithIncludeYMLFile', 'docassemble.VTFeedback', 'docassemble.VTSharedYMLFile'],
+      install_requires=['docassemble.AssemblyLine @ git+https://github.com/SuffolkLITLab/docassemble-AssemblyLine.git@main', 'docassemble.GithubFeedbackForm @ git+https://github.com/SuffolkLITLab/docassemble-GithubFeedbackForm.git@main', 'docassemble.VTDisclosureOfExemptIncome @ git+https://github.com/LSVermont/docassemble-VTDisclosureOfExemptIncome.git@main', 'docassemble.VTFeeWaiverWithIncludeYMLFile @ git+https://github.com/VTskier/docassemble-VTFeeWaiverWithIncludeYMLFile.git@main', 'docassemble.VTFeedback @ git+https://github.com/VTskier/docassemble-VTFeedback.git@main', 'docassemble.VTSharedYMLFile @ git+https://github.com/LSVermont/docassemble-VTSharedYMLFile.git@main'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/VTSmallClaimsAnswer/', package='docassemble.VTSmallClaimsAnswer'),
      )
